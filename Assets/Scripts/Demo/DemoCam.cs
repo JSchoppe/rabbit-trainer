48c4012a19e04a13e16579e6e09069be9ffc6420
JSchoppe <jethro.schoppenhorst@gmail.com>
2020-08-17 21:39:32 -0500

    Added Demonstration Camera
    Added a camera that can be enabled to watch over an agents shoulder as they train

---
+++ Assets/Scripts/Demo/DemoCam.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+// The script sidesteps direct parenting so that the camera can exist without
+// reorganizing the hierarchy.
+
+/// <summary>Simple camera script for recording the agent</summary>
+[RequireComponent(typeof(Camera))]
+public sealed class DemoCam : MonoBehaviour
+{
+    [SerializeField] private bool isFollowingAgent = true;
+
+    [SerializeField] private Transform targetAgent = null;
+    [SerializeField] private float distanceBehind = 6;
+    [SerializeField] private float distanceAbove = 3;
+
+    private void Start()
+    {
+        if(isFollowingAgent)
+            StartCoroutine(TrackCamera());
+    }
+
+    private IEnumerator TrackCamera()
+    {
+        while(true)
+        {
+            transform.position = targetAgent.position - targetAgent.forward * distanceBehind + targetAgent.up * distanceAbove;
+            transform.LookAt(targetAgent);
+            yield return null;
+        }
+    }
+}
